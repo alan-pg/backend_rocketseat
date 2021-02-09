b9f4cd7fadd6b9a0e26fe21ee121485b4d4436a3
Alan <alan92goncalves@gemail.com>
2021-02-09 10:17:39 -0300

    fim

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,20 @@
 import 'reflect-metadata';
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 import uploadConfig from './config/aupload';
 
 import './database';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.get('/teste', (req, res) => {
+  return res.json({ resp: 'olaa'});
+})
 
 app.listen(3333, () => {
   console.log('Server started on port 3333 ✌');
